e2d3575ea5817b4980626a3650778a6c10fbf478
Naymur000 <nayeemur000@gmail.com>
2026-01-30 20:16:22 +0600

    u

---
+++ src/Demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Demo = () => {
+    return (
+        <div>
+            <h1>I am learning react</h1>
+            <input placeholder="My name" />
+            <button>Submit</button>
+        </div>
+    );
+};
+
+export default Demo;
---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,10 @@
+
+const ProfilePage = () => {
+    return (
+        <div>
+            This is Profile page
+        </div>
+    );
+};
+
+export default ProfilePage;
